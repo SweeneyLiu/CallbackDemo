187855e1a8086dc7e500f21472f3d671d6b17ece
sweeneyliu <sweeneyliu@tencent.com>
2019-03-04 15:11:52 +0800

    增加注释

--- app/src/main/java/com/lsw/callbackdemo/MainActivity.java
+++ app/src/main/java/com/lsw/callbackdemo/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * 例如程序员 A 和 B 之间需要协作，A 告诉 B 这个任务中间的某个环节需要 B 来完成，并且完成后告诉 A，
+ * 这时候程序员 A 就需要告诉 B 一个联系方式，使 B 完成时来通知自己，这个场景就可以使用回调。
+ */
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
